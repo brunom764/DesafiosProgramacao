2f5df9a450ed3b9a86458027c5c74ec52136b2f1
Bruno Moreira <100159869+brunom764@users.noreply.github.com>
2023-02-21 14:09:17 -0300

    add desc

--- vetores, fila e pilha/pilhaCaderno.py
+++ vetores, fila e pilha/pilhaCaderno.py
@@ -1,17 +1,12 @@
 # Resumo do problema: Confira se todas as frentes (F) tem seus devidos versos (V) em uma ordem satisfatória.
 # Toda frente precisa ter um verso depois dela, não necessariamente imediatamente após, mas eventualmente.
 # Nenhum verso pode aparecer sem que uma frente tenha o antecedido em algum momento.
-# E, atenção, uma pilha que tem a mesma quantidade de frentes e versos NÃO está necessariamente correta.
-#
-# OBS: Strings vazias devem ser consideradas corretas.
-#
+
 # Input
-#
 # A única linha de entrada é composta por uma string, que corresponde a pilha de capas manuseada pelo funcionário.
 # A string é uma sequência de letras sem espaços ou vírgulas então elas, como VFVFFFVVFVFV.
-#
+
 # Output
-#
 # Você deve produzir uma única linha de saída com a expressão “Correto.”
 # caso a pilha seja bem formada, e “Incorreto, devido a capa na posição X.” caso contrário,
 # onde X é a posição da primeira capa que interfere na integridade do caderno.
